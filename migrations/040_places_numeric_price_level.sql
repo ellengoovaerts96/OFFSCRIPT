a5f5612c8b65d7e23c230fa11a597c18020460c8
ellengoovaerts96 <goovaerts.ellen@telenet.be>
2026-07-20 22:49:31 +0200

    Make price migration idempotent

--- migrations/040_places_numeric_price_level.sql
+++ migrations/040_places_numeric_price_level.sql
@@ -1,12 +1,12 @@
 ALTER TABLE public.places
   ALTER COLUMN price_level TYPE SMALLINT
   USING CASE
-    WHEN price_level IS NULL OR btrim(price_level) = '' THEN NULL
-    WHEN lower(btrim(price_level)) IN ('1', 'budget', 'low', '€', '$') THEN 1
-    WHEN lower(btrim(price_level)) IN ('2', 'affordable', 'betaalbaar', '€€', '$$') THEN 2
-    WHEN lower(btrim(price_level)) IN ('3', 'average', 'medium', 'mid-range', 'gemiddeld') THEN 3
-    WHEN lower(btrim(price_level)) IN ('4', 'chic', 'high', 'upscale', '€€€', '$$$') THEN 4
-    WHEN lower(btrim(price_level)) IN ('5', 'luxury', 'luxe', '€€€€', '$$$$') THEN 5
+    WHEN price_level IS NULL OR btrim(price_level::text) = '' THEN NULL
+    WHEN lower(btrim(price_level::text)) IN ('1', 'budget', 'low', '€', '$') THEN 1
+    WHEN lower(btrim(price_level::text)) IN ('2', 'affordable', 'betaalbaar', '€€', '$$') THEN 2
+    WHEN lower(btrim(price_level::text)) IN ('3', 'average', 'medium', 'mid-range', 'gemiddeld') THEN 3
+    WHEN lower(btrim(price_level::text)) IN ('4', 'chic', 'high', 'upscale', '€€€', '$$$') THEN 4
+    WHEN lower(btrim(price_level::text)) IN ('5', 'luxury', 'luxe', '€€€€', '$$$$') THEN 5
     ELSE NULL
   END;
 
